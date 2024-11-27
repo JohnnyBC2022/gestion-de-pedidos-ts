b3f9cdd7ca4bd963952f0dbddf87ffcd585daa3d
Johnny <jonathanbc2012@gmail.com>
2024-11-27 20:20:28 +0100

    feat: MenuItem component created

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,8 +1,14 @@
+import { menuItems } from "../data/db";
+import MenuItem from "./MenuItem";
+
 const Main = () => {
   return (
     <main className="max-w-7xl mx-auto py-20 grid md:grid-cols-2">
       <div>
         <h2>Menú</h2>
+        {menuItems.map((item) => (
+          <MenuItem key={item.id} />
+        ))}
       </div>
 
       <div>
