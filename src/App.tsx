ccd98c296fb3e08d8af1cee9bf661f8ffbc2784e
Johnny <jonathanbc2012@gmail.com>
2024-11-27 20:02:03 +0100

    feat: database created with types

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import Header from "./components/Header";
-
+import { menuItems } from "./data/db";
 
 function App() {
   
+  console.log(menuItems);
 
   return (
     <>
---
+++ src/data/db.ts
@@ -0,0 +1,64 @@
+import type { MenuItem } from '../types'
+
+export const menuItems : MenuItem[] = [
+    {
+      "id": 1,
+      "name": "Pizza a la Leña Pequeña",
+      "price": 12
+    },
+    {
+      "id": 2,
+      "name": "Pizza a la Leña Mediana",
+      "price": 15
+    },
+    {
+      "id": 3,
+      "name": "Rebanada de Pan de Ajo",
+      "price": 3
+    },
+    {
+      "id": 4,
+      "name": "Rebanada de Pastel de Chocolate",
+      "price": 7
+    },
+    {
+      "id": 5,
+      "name": "Zumo de Naranja Natural",
+      "price": 5
+    },
+    {
+      "id": 6,
+      "name": "Pizza a la Leña Grande",
+      "price": 18
+    },
+    {
+      "id": 7,
+      "name": "Costillar de Cerdo 800g",
+      "price": 25
+    },
+    {
+      "id": 8,
+      "name": "Smoothie de Frutas del Bosque",
+      "price": 5
+    },
+    {
+      "id": 9,
+      "name": "Tequila",
+      "price": 8
+    },
+    {
+      "id": 10,
+      "name": "Tarta de Queso con Arándanos",
+      "price": 6
+    },
+    {
+      "id": 11,
+      "name": "Café Americano",
+      "price": 2
+    },
+    {
+      "id": 12,
+      "name": "Café Capuchino",
+      "price": 4
+    }
+]
